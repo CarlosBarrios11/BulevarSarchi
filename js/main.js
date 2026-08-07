d83730f72ddbfb530e75bb5ce0e516c007757148
gimena <gcuberoarias@gmail.com>
2026-08-07 12:23:08 -0600

    creación de el header y se creo el archivo en java

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// funciones para que el menu hamburgesa si funciene sino  nunca sale
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
