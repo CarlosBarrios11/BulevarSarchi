9af115e1f3876ba1b8d49946a65300407549a35a
gimena <gcuberoarias@gmail.com>
2026-08-08 16:50:00 -0600

    Actualización del funcionamiento del menú hamburgusa

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
+//Menú hamburguesa
+const abrir = document.getElementById('abrir');
+const cerrar = document.getElementById('cerrar');
+const nav = document.getElementById('nav');
+const overlay = document.getElementById('overlay');
 
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-});
 
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-});
+function abrirMenu() {
+
+    //1. Abre el menú lateral
+    nav.classList.add('visible');
+    //2. Crea una máscara por detrás del nav
+    overlay.classList.add('active');
+    //3. Restringe el scroll
+    document.body.classList.add("bodyNoScroll")
+    
+}
+
+function cerrarMenu() {
+    nav.classList.remove('visible');
+    overlay.classList.remove('active');
+    document.body.classList.remove("bodyNoScroll")
+}
+
+
+// Función para confirmar si el click fue dentro del contenedor del nav
+function manejarClickOverlay(e) {
+    if (e.target === overlay) {
+        cerrarMenu();
+    }
+}
+
+
+//Llamado de funciones
+cerrar.addEventListener('click', cerrarMenu);
+abrir.addEventListener('click', abrirMenu);
+overlay.addEventListener('click', cerrarMenu);
